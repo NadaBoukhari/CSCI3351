094327d9a00145783df3b972cefbb023d82e7591
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-10-17 13:51:35 +0100

    formatted directory prompt

--- Boukhari_Rajallah.c
+++ Boukhari_Rajallah.c
@@ -4,7 +4,7 @@
 
 
 // In order to read user input, we've used the "readline" library for GNU that can be installed using    sudo apt-get install libreadline-dev
-// To create the binary file, please use the command     gcc assignment.c -lreadline    
+// To create the binary file, please use the command     gcc Boukhari_Rajallah.c -lreadline    
 // To run the program please run      ./a.out
 
 #include<stdio.h> 
@@ -14,15 +14,30 @@
 #include<string.h> 
 #include<unistd.h> 
 #include<readline/readline.h> 
+#include <limits.h>
 
 
+char* getDirectoryPrompt(){
+	
+	char cwd[PATH_MAX];
+	char temp[PATH_MAX];	
+	char* directory;
+	
+	// get current working directory and format it like a default shell prompt
+	getcwd(cwd, sizeof(cwd));
+	strcpy(temp, "\n");
+	directory = strcat(strcat(temp, cwd),"$ ");
+
+	return directory;
+}
 // Read the input by the user
 int scanUserInput(char* str) 
-{ 
+{ 	
 	char* strLine; 
-	strLine = readline("\n>>> "); 
+	
+	strLine = readline(getDirectoryPrompt()); 
 	if (strlen(strLine) != 0) {   // Checking if there was indeed an input or is empty
-		strcpy(str, strLine);  //Once confirmed, we store back the input line into the str pointer (pass by referrence)
+		strcpy(str, strLine);  // Once confirmed, we store back the input line into the str pointer (pass by referrence)
 		return 0; 
 	} else { 
 		return 1; 
@@ -41,12 +56,12 @@ void execArgs(char** commandToExec)
 		return; 
 	} else if (childProcess == 0) { 
     
-		if (execvp(commandToExec[0], commandToExec) < 0) {  //Command executed successfully
+		if (execvp(commandToExec[0], commandToExec) < 0) {  // Command executed successfully
 			printf("\nNo command found"); 
 		} 
 		exit(0); 
 	} else { 
-		// waiting for child to terminate 
+		// Waiting for child to terminate 
 		wait(NULL); 
 		return; 
 	} 
@@ -83,17 +98,17 @@ int nonExecutablesCMDs(char** parsed)
 } 
 
 
-// function for parsing command words 
+// Function for parsing command words 
 void parseSpace(char* str, char** parsed) 
 { 
 	int i; 
 
-	for (i = 0; i < 100; i++) {  //Setting max to 100 since max number of args to handle in our case is 100
-		parsed[i] = strsep(&str, " "); //separating on a space delimiter basis
+	for (i = 0; i < 100; i++) {  // Setting max to 100 since max number of args to handle in our case is 100
+		parsed[i] = strsep(&str, " "); // Separating on a space delimiter basis
 
 		if (parsed[i] == NULL) 
 			break; 
-		if (strlen(parsed[i]) == 0)  //Overwrite this item on the next iteration. This happens when there is space before a command.
+		if (strlen(parsed[i]) == 0)  // Overwrite this item on the next iteration. This happens when there is space before a command.
 			i--; 
 	} 
 } 
@@ -106,15 +121,15 @@ int main()
 
 	while (1) { 
 		
-		// take input 
+		// Take input 
 		if (scanUserInput(userInp)) {
 			continue; 
         }
         
-		//process input
+		// Process input
         parseSpace(userInp, parsedInp); 
 
-		//execute command + args
+		// Execute command + args
         if (nonExecutablesCMDs(parsedInp) == 0) 
             execArgs(parsedInp); 
 	} 
